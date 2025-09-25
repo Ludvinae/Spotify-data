d476fc678cf619be9e97842f4000e89874616d1b
Ludvinae <benoit.champlon79@gmail.com>
2025-09-25 21:37:57 +0200

    feat(main): loop through dataset to gather risk users and add tham to a list

--- spotify.py
+++ spotify.py
@@ -1,5 +1,6 @@
 import csv
 from churned import displayAttrition
+from riskUsers import isRiskUser
 
 def read_spotify_data(file_path, debug=True):
     spotify_data = []
@@ -20,3 +21,10 @@ dataset = read_spotify_data(file_path="spotify_churn_dataset.csv", debug=False)
 
 # Attrition is the rate of "is_churned" divided by the total of users
 displayAttrition(dataset)
+
+# Check the dataset for risk users and add them to a list
+riskUsers = []
+for user in dataset:
+    if isRiskUser(user):
+        riskUsers.append(user)
+        print(user)
