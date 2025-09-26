ebe7e194462c96c724b14e44e2aaff8cf23df8f9
Ludvinae <benoit.champlon79@gmail.com>
2025-09-26 10:57:35 +0200

    feat(revenus): added functions to manage the list of disctionnaries for storing data on sub types

--- revenus.py
+++ revenus.py
@@ -8,3 +8,30 @@ PRICES = {
 }
 '''
 
+def isNotChurned(user):
+    if user["is_churned"] == "0":
+        return True
+    return False
+
+subTypeCount = [] * 4
+
+def subType(user):
+    match user["subscription_type"]:
+        case "Free":
+            subTypeCount[0] += 1
+        case "Premium":
+            subTypeCount[1] += 1
+        case "Family":
+            subTypeCount[2] += 1
+        case "Student":
+            subTypeCount[3] += 1
+
+countries = {}
+
+def newCountryCode(user):
+    countries[user["country"]] = [0, 0, 0, 0]
+
+def countryDict(user):
+    if user["country"] not in countries:
+        newCountryCode(user)
+    countries[user["country"]] = subType(user)
