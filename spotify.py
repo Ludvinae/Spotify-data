883a00d4421a78f9bd1fdef0449864a63ba6d461
Ludvinae <benoit.champlon79@gmail.com>
2025-09-29 16:23:43 +0200

    feat(main): wrote a function to ask user what function he wants to run

--- spotify.py
+++ spotify.py
@@ -24,34 +24,67 @@ def read_spotify_data(file_path, debug=True):
 dataset = read_spotify_data(file_path="spotify_churn_dataset.csv", debug=False)
 
 
-# Attrition is the rate of "is_churned" divided by the total of users
-displayAttrition(dataset)
 
-# Check the dataset for risk users and add them to a list
-riskUsers = []
-riskUsers2 = []
-count = 0
-count2 = 0
-for user in dataset:
-    if isRiskUser(user):
-        riskUsers.append(user)
-        count += 1
-    if altRiskUser(user):
-        riskUsers2.append(user)
-        count2 += 1
 
-print(count / len(dataset))
-print(count2 / len(dataset))
-print(len(riskUsers))
-print(len(riskUsers2))
 
-getRevenusByCountry(dataset)
 
-powerUsers = getPowerUsers(dataset)
-print(f"Power Users count : {len(powerUsers)}")
 
-adsPerSub(dataset)
 
-deviceMix(dataset)
 
-ageBracket(dataset)
+
+
+
+
+
+
+
+
+
+
+while True:
+    choice = input("Quelle fonction voulait vous tester? (Q pour arrêter, 1-10 pour choisir une fonction): ")
+    match choice:
+        case "1":
+            # Attrition is the rate of "is_churned" divided by the total of users
+            displayAttrition(dataset)
+        case "2":
+            # Check the dataset for risk users and add them to a list
+            riskUsers = []
+            riskUsers2 = []
+            count = 0
+            count2 = 0
+            for user in dataset:
+                if isRiskUser(user):
+                    riskUsers.append(user)
+                    count += 1
+                if altRiskUser(user):
+                    riskUsers2.append(user)
+                    count2 += 1
+
+            print(count / len(dataset))
+            print(count2 / len(dataset))
+            print(len(riskUsers))
+            print(len(riskUsers2))
+        case "3":
+            getRevenusByCountry(dataset)
+        case "4":
+            powerUsers = getPowerUsers(dataset)
+            print(f"Power Users count : {len(powerUsers)}")
+        case "5":
+            adsPerSub(dataset)
+        case "6":
+            deviceMix(dataset)
+        case "7":
+            ageBracket(dataset)
+        case "8":
+            pass
+        case "9":
+            pass
+        case "10":
+            pass
+        case "q" | "Q":
+            break
+        case _:
+            print("wrong input, try again or type 'Q' to abort")
+
+
