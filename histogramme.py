bf8df1620b594df16f9ac9e184f8fb8d9ab7fe77
Ludvinae <benoit.champlon79@gmail.com>
2025-09-30 20:35:03 +0200

    fix(histogramme): converted string to float to fix the buggy display

--- histogramme.py
+++ histogramme.py
@@ -6,15 +6,14 @@ import matplotlib.pyplot as plt
 def histogramme(data):
     skipRate = []
     for user in data:
-        skipRate.append(user["skip_rate"])
+        skipRate.append(float(user["skip_rate"]))
 
     #skipRate.sort()
     showHistogram(skipRate)
     
 
 def showHistogram(list):
-    
-    plt.hist(list, bins=10, edgecolor="black", rwidth=0.8)
+    plt.hist(list, bins=12, edgecolor="black", rwidth=0.75, color="#B9C1E6")
     plt.xlabel("skip rate value")
     plt.ylabel("number of occurence")
 
