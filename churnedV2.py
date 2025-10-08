89dde75428197695477cc1806ccad29848fd67eb
Ludvinae <benoit.champlon79@gmail.com>
2025-10-08 10:57:19 +0200

    refactor(churned): changed the way to get the churned user with filter instead of loop

---
+++ churnedV2.py
@@ -0,0 +1,30 @@
+
+churnByType = {}
+totalByType = {}
+
+
+def churned(data):
+    churnedList = filter(filterChurned, data)
+    for user in churnedList:
+        addToChurnedDict(user["subscription_type"])
+        
+    total = 0
+    for subType in churnByType:
+        total += int(churnByType[subType])
+        totalPerSubType = filter(lambda x: x["subscription_type"] == subType , data)
+        totalByType[subType] = f"{(float(churnByType[subType]) / len(list(totalPerSubType))) * 100:.2f} %"
+
+    totalByType["Total"] = f"{(float(total) / len(data)) * 100:.2f} %"
+    
+    return totalByType
+
+def filterChurned(user):
+    if user["is_churned"] == "1":
+        return True
+    return False
+
+def addToChurnedDict(subType):
+    if subType not in churnByType:
+        churnByType[subType] = 0
+    churnByType[subType] += 1
+
